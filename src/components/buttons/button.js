06a85f1e7619fe3d5dcdfb43f6a4f0383640846b
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-09-03 16:43:29 -0500

    fixed misspelled css bug and centered button text

--- src/components/buttons/button.js
+++ src/components/buttons/button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './button.css';
 
 function Button({label}){
-    return <div data-testid="button">{label}</div>
+    return <div data-testid="button" className="button-style">{label}</div>
 }
 
 export default Button;
